f7a379331cf5d57cbf9aac0a563a5a5cfd234b34
kovacsnorb <kovacsnorb@gmail.com>
2017-09-28 13:14:03 +0200

    feat(List tasks): code written, no review yet

--- ToDoApp/ToDoApp/Program.cs
+++ ToDoApp/ToDoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,10 @@ namespace ToDoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n  Command Line Todo application" +
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\n  Command Line Todo application" +
                 "\n  =============================" +
                 "\n" +
                 "\n  Command line arguments:" +
@@ -18,6 +22,28 @@ namespace ToDoApp
                 "\n  -a   Adds a new task" +
                 "\n  -r   Removes a task" +
                 "\n  -c   Completes a task");
+            }
+
+            if (args.Contains("-l"))
+            {
+                string path = @"./todolist.txt";
+                try
+                {
+                    Console.WriteLine("\n");
+                    string[] content = File.ReadAllLines(path);
+                    List<string[]> contentDetailed = new List<string[]>();
+
+                    for (int i = 0; i < content.Count(); i++)
+                    {
+                        contentDetailed.Add(content[i].Split(';'));
+                        Console.WriteLine("  " + (i + 1) + " - " + contentDetailed[i][0]);
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("Could not read the file!");
+                }
+            }
         }
     }
 }
